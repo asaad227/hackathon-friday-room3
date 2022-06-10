c562fcb84b9b9b2b1f96cf7c1c19778811c1db43
Victoria Khatian <khatian.v@gmail.com>
2022-06-10 13:35:56 +0100

    limit for products

--- bestshop/src/Components/Home/Home.js
+++ bestshop/src/Components/Home/Home.js
@@ -4,9 +4,7 @@ function Home() {
   const [data, setData] = useState([]);
 
   async function getApi() {
-    const response = await fetch(
-      `https://fakestoreapi.com/products/categories`
-    );
+    const response = await fetch(`https://fakestoreapi.com/products?limit=9`);
     const data = await response.json();
     console.log(data);
     if (data.status === 404) {
@@ -22,17 +20,15 @@ function Home() {
 
   return (
     <div>
-      {data.map((e, index) => {
-        if (index < 10) {
-          return (
-            <div key={e.id}>
-              <h4>{e.title}</h4>
-              <img src={e.image} width={200} alt={e.title} />
-              <h5>{e.category}</h5>
-              <p>{e.description}</p>
-            </div>
-          );
-        }
+      {data.map((e) => {
+        return (
+          <div key={e.id}>
+            <h4>{e.title}</h4>
+            <img src={e.image} width={200} alt={e.title} />
+            <h5>{e.category}</h5>
+            <p>{e.description}</p>
+          </div>
+        );
       })}
     </div>
   );
